29a51145dddba146fc2bb1b080d8416a69ee6a8b
Mradiemperor <adityasjc31@gmail.com>
2025-12-11 15:07:47 +0530

    Copied exception folder from InventoryManagementModule

---
+++ src/main/java/com/db/ms/exception/InsufficientStockException.java
@@ -0,0 +1,19 @@
+package com.db.ms.exception;
+/**
+ * Exception thrown when insufficient stock is available for an operation.
+ *
+ * @author Aditya Srivastava
+ * @version 1.0
+ * @since 2024-12-08
+ */
+public class InsufficientStockException extends RuntimeException {
+
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+
+    public InsufficientStockException(Long bookId, Integer available, Integer requested) {
+        super(String.format("Insufficient stock for book ID: %d. Available: %d, Requested: %d",
+                bookId, available, requested));
+    }
+}
---
+++ src/main/java/com/db/ms/exception/InvalidInventoryOperationException.java
@@ -0,0 +1,14 @@
+package com.db.ms.exception;
+/**
+ * Exception thrown for invalid inventory operations.
+ *
+ * @author Aditya Srivastava
+ * @version 1.0
+ * @since 2024-12-08
+ */
+public class InvalidInventoryOperationException extends RuntimeException {
+
+    public InvalidInventoryOperationException(String message) {
+        super(message);
+    }
+}
