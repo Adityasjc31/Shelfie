4989632b454fbf2dbdcd1a7e23248ab3957a45d0
2460600 <Rehan.Ashraf@cognizant.com>
2025-12-15 17:47:00 +0530

    feat : renamed Order -> order

--- src/main/java/com/db/ms/Order/exception/OrderCancellationNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.db.ms.Order.exception;
-
-public class OrderCancellationNotAllowedException extends RuntimeException {
-    public OrderCancellationNotAllowedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/db/ms/Order/exception/OrderException.java
+++
@@ -1,5 +0,0 @@
-package com.db.ms.Order.exception;
-
-
-public class OrderException extends Exception{
-}
