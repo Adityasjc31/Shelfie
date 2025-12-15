4d1b5de6da675857ff3fd6849513cb79bbe0680c
Aditya Srivastava <adityasjc31@gmail.com>
2025-12-15 20:17:40 +0530

    deleted inventory module

--- src/test/java/com/db/ms/Inventory/controller/InventoryControllerTest.java
+++ src/test/java/com/db/ms/Inventory/controller/InventoryControllerTest.java
@@ -10,9 +10,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.db.ms.inventory.controller.InventoryController;
-import com.db.ms.inventory.dto.*;
-import com.db.ms.inventory.service.InventoryService;
+import com.db.ms.Inventory.controller.InventoryController;
+import com.db.ms.Inventory.dto.*;
+import com.db.ms.Inventory.service.InventoryService;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
