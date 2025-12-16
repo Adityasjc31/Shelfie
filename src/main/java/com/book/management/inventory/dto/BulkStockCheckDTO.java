5ce2156546c6c54099db40bcfdcaf563b9eadeda
Mradiemperor <adityasjc31@gmail.com>
2025-12-16 11:13:51 +0530

    import correction in bulk

--- src/main/java/com/book/management/inventory/dto/BulkStockCheckDTO.java
+++ src/main/java/com/book/management/inventory/dto/BulkStockCheckDTO.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import java.util.Map;
 
