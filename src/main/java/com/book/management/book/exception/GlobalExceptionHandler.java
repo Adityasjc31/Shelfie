cc4ed6df6f89d6ca0e08c2c1c365afec34a3d3f1
Mradiemperor <adityasjc31@gmail.com>
2025-12-16 12:56:34 +0530

    few manual refactoring after merge

--- src/main/java/com/book/management/book/exception/GlobalExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.book.management.book.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalBookExceptionHandler {
-
-}
