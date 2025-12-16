bca2889a0dd2f08bdc20ce730f9b4d36835472fa
Mradiemperor <adityasjc31@gmail.com>
2025-12-16 11:37:28 +0530

    state change after merge

--- src/main/java/com/book/management/book/config/AppConfig.java
+++ src/main/java/com/book/management/book/config/AppConfig.java
@@ -1,12 +1,7 @@
-<<<<<<<< HEAD:src/main/java/com/book/management/user/config/AppConfig.java
-package com.book.management.user.config;
-========
 package com.book.management.book.config;
->>>>>>>> origin/ServiceCall_Book_Inventory_Order_Integration:src/main/java/com/book/management/book/config/AppConfig.java
 
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 
 @Configuration
 public class AppConfig {
