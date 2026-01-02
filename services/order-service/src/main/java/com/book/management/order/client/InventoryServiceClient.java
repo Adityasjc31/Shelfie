8fb7b661d21a996e5a00aba6b3fcc6e4d0482446
2460600 <Rehan.Ashraf@cognizant.com>
2026-01-03 01:04:05 +0530

    order-service :refactored InventoryServiceClient

--- services/order-service/src/main/java/com/book/management/order/client/InventoryServiceClient.java
+++ services/order-service/src/main/java/com/book/management/order/client/InventoryServiceClient.java
@@ -3,7 +3,7 @@ package com.book.management.order.client;
 
 import com.book.management.order.client.fallback.InventoryClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
@@ -14,6 +14,6 @@ import java.util.Map;
  */
 @FeignClient(name = "inventory-service", path = "/api/v1/inventory", fallback = InventoryClientFallback.class)
 public interface InventoryServiceClient {
-    @PostMapping("/reduce")
+    @PatchMapping("/reduce")
     void reduceStock(@RequestBody Map<Long, Integer> itemsToReduce);
 }
